631ffef1892d66444ecc822d6f28e9ad1e7e15cc
toponlineform <toponlineforms@gmail.com>
2025-12-20 07:23:47 +0530

    Update answerKeyBox.js

--- src/data/home/answerKeyBox.js
+++ src/data/home/answerKeyBox.js
@@ -1,12 +1,13 @@
 export const answerKeyBox = {
-  id: "results",
-  title: "Results",
-  color: "green",
-  viewMoreLink: "/results",
+  id: "answer-key",
+  title: "Answer Key",
+  color: "purple",
+  viewMoreLink: "/answer-key",
 
     // ✅ BAS YE 2 LINE ADD KARNI HAIN AUTOMATIC KE LIYE:
   auto: true,               // Switch ON and Use false for OFF Automation
   categoryName: "Latest Jobs",   // Master Data wali Category ka naam match hona chahiye
+  
   links: [
     { title: "Bihar Board 12th Result", url: "#", tag: "Declared", tagColor: "red" },
     { title: "SSC CGL Tier 1 Result", url: "#" }
