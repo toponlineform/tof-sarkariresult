745942e275ebccd7e627edb582b10627766142de
toponlineform <toponlineforms@gmail.com>
2025-12-21 03:06:43 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,23 @@
 // 1. Sirf Latest Jobs ko import karein
 import { latestJobs } from './latestJobs';
 import { admitCard } from './admitCard';
+import { results } from './results';
+import { admission } from './admission';
+import { answerKey } from './answerKey';
 
 // 2. Master List (Abhi isme sirf jobs hain)
 export const allPosts = [
-  ...latestJobs
-  ...admitCard
+  ...latestJobs,
+  ...admitCard,
+  ...results,
+  ...admission,
+  ...answerKey
 ];
 
 // 3. Export for Home Page
 export { latestJobs };
 export { admitCard };
+export { results };
+export { admission };
+export { answerKey };
 
