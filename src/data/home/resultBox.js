1c39ab04787dd2ba6213085b2738d187e87975ec
toponlineform <toponlineforms@gmail.com>
2025-12-20 05:49:59 +0530

    Create resultBox.js

---
+++ src/data/home/resultBox.js
@@ -0,0 +1,10 @@
+export const resultBox = {
+  id: "results",
+  title: "Results",
+  color: "green",
+  viewMoreLink: "/results",
+  links: [
+    { title: "Bihar Board 12th Result", url: "#", tag: "Declared", tagColor: "red" },
+    { title: "SSC CGL Tier 1 Result", url: "#" }
+  ]
+};
