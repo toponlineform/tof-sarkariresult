8164f9bcc474586d97e1d089ec979a5a2dc8522e
toponlineform <toponlineforms@gmail.com>
2025-12-20 05:54:13 +0530

    Create syllabusBox.js

---
+++ src/data/home/syllabusBox.js
@@ -0,0 +1,10 @@
+export const syllabusBox = {
+  id: "results",
+  title: "Results",
+  color: "green",
+  viewMoreLink: "/results",
+  links: [
+    { title: "Bihar Board 12th Result", url: "#", tag: "Declared", tagColor: "red" },
+    { title: "SSC CGL Tier 1 Result", url: "#" }
+  ]
+};
