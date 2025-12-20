14b76a31d62ed79fd29d234e6779cfe6c24b6b30
toponlineform <toponlineforms@gmail.com>
2025-12-20 07:14:12 +0530

    Update admitCardBox.js

--- src/data/home/admitCardBox.js
+++ src/data/home/admitCardBox.js
@@ -3,6 +3,10 @@ export const admitCardBox = {
   title: "Admit Card",
   color: "blue",
   viewMoreLink: "/admit-card",
+    // ✅ BAS YE 2 LINE ADD KARNI HAIN AUTOMATIC KE LIYE:
+  auto: true,               // Switch ON and Use false for OFF Automation
+  categoryName: "Latest Jobs",   // Master Data wali Category ka naam match hona chahiye
+  
   links: [
     { title: "UPSC Prelims Admit Card", url: "#", tag: "Released", tagColor: "green" },
     { title: "CTET Jan 2025 Hall Ticket", url: "#" }
