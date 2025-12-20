3b7aa6003b5fc3c585cbb5d44592a09603624792
toponlineform <toponlineforms@gmail.com>
2025-12-20 05:56:22 +0530

    Create index.js

---
+++ src/data/home/index.js
@@ -0,0 +1,16 @@
+import { latestJobsBox } from './latestJobsBox';
+import { admitCardBox } from './admitCardBox';
+import { resultBox } from './resultBox';
+import { answerKeyBox } from './answerKeyBox';
+import { admissionBox } from './admissionBox';
+import { syllabusBox } from './syllabusBox';
+
+// Yahan decide karein ki kaunsa box pehle dikhega
+export const homeBoxes = [
+  latestJobsBox,
+  admitCardBox,
+  resultBox,
+  answerKeyBox,
+  admissionBox,
+  syllabusBox
+];
