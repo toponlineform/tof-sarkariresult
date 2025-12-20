5a7d7561984033babec1c3655b49083ea988373a
toponlineform <toponlineforms@gmail.com>
2025-12-20 05:47:09 +0530

    Create latestJobsBox.js

---
+++ src/data/home/latestJobsBox.js
@@ -0,0 +1,19 @@
+export const latestJobsBox = {
+  id: "latest-jobs",
+  title: "Latest Jobs",
+  color: "red",
+  viewMoreLink: "/latest-jobs",
+  links: [
+    { 
+      title: "UP Police Constable Recruitment 2025", 
+      url: "/job/up-police-constable-2025", 
+      tag: "Last Date Today", tagColor: "red", emoji: "🔥" 
+    },
+    { 
+      title: "SSC GD Constable 2025", 
+      url: "/job/ssc-gd-constable-2025", 
+      tag: "New", tagColor: "green" 
+    }
+    // Aur jobs yahan add karein...
+  ]
+};
