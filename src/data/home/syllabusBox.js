7c136d2becbb80418e842a32974b06953f20e2f1
toponlineform <toponlineforms@gmail.com>
2025-12-20 07:19:54 +0530

    Update syllabusBox.js

--- src/data/home/syllabusBox.js
+++ src/data/home/syllabusBox.js
@@ -1,8 +1,13 @@
 export const syllabusBox = {
-  id: "results",
-  title: "Results",
-  color: "green",
+  id: "syllabus",
+  title: "Syllabus",
+  color: "pink",
   viewMoreLink: "/results",
+
+    // ✅ BAS YE 2 LINE ADD KARNI HAIN AUTOMATIC KE LIYE:
+  auto: true,               // Switch ON and Use false for OFF Automation
+  categoryName: "Latest Jobs",   // Master Data wali Category ka naam match hona chahiye
+  
   links: [
     { title: "Bihar Board 12th Result", url: "#", tag: "Declared", tagColor: "red" },
     { title: "SSC CGL Tier 1 Result", url: "#" }
