d56e740c36744115108cd05daae5966e8c2090ca
toponlineform <toponlineforms@gmail.com>
2025-12-20 07:13:35 +0530

    Update admissionBox.js

--- src/data/home/admissionBox.js
+++ src/data/home/admissionBox.js
@@ -3,6 +3,10 @@ export const admissionBox = {
   title: "Results",
   color: "green",
   viewMoreLink: "/results",
+    // ✅ BAS YE 2 LINE ADD KARNI HAIN AUTOMATIC KE LIYE:
+  auto: true,               // Switch ON and Use false for OFF Automation
+  categoryName: "Latest Jobs",   // Master Data wali Category ka naam match hona chahiye
+  
   links: [
     { title: "Bihar Board 12th Result", url: "#", tag: "Declared", tagColor: "red" },
     { title: "SSC CGL Tier 1 Result", url: "#" }
