7b58dc53ceb33d470c85e4cd5fb56adf62a92f87
toponlineform <toponlineforms@gmail.com>
2025-12-20 05:48:13 +0530

    Create admitCardBox.js

---
+++ src/data/home/admitCardBox.js
@@ -0,0 +1,10 @@
+export const admitCardBox = {
+  id: "admit-card",
+  title: "Admit Card",
+  color: "blue",
+  viewMoreLink: "/admit-card",
+  links: [
+    { title: "UPSC Prelims Admit Card", url: "#", tag: "Released", tagColor: "green" },
+    { title: "CTET Jan 2025 Hall Ticket", url: "#" }
+  ]
+};
